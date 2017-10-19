337c9202f3f788b96403a98c8921240a6b2faab9
Matthias Michael Loker <m@theloker.com>
2017-10-19 16:56:26 +0200

    Add stfs route namespace

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,13 @@
 class App < Rack::App
   extend Rack::App::FrontEnd
 
-  get '/' do
-    if !params['q'].nil?
-      res = CONN.exec("SELECT * FROM episodes WHERE to_tsvector(title || '. ' || description) @@ plainto_tsquery('#{params['q']}');") # TODO: NEVER STRING INTERPOLATE USERINPUT DIRECTLY IN A DATABASE QUERY
-      @episodes = res.map{|record| Episode.new(title: record['title'], publishing_date: record['publishing_date'], description: record['description'])}
+  namespace '/stfs' do
+    get '/' do
+      if !params['q'].nil?
+        res = CONN.exec("SELECT * FROM episodes WHERE to_tsvector(title || '. ' || description) @@ plainto_tsquery('#{params['q']}');") # TODO: NEVER STRING INTERPOLATE USERINPUT DIRECTLY IN A DATABASE QUERY
+        @episodes = res.map{|record| Episode.new(title: record['title'], publishing_date: record['publishing_date'], description: record['description'])}
+      end
+      render './views/index.html.erb'
     end
-    render './views/index.html.erb'
   end
 end
