354c0d3145b12b8831b00f4c8186d3569c08c51a
Matthias Michael Loker <m@theloker.com>
2017-10-31 11:30:15 +0100

    Downgrade postgres to v9.5
    The postgresql alpine package contains an older version of pg_dump
    which leads to the following error when dumping the database schema
    
    pg_dump: server version: 10.0; pg_dump version: 9.5.8
    pg_dump: aborting because of server version mismatch
    
    using postgres v9.5.x solves this issue

--- bin/init_db.sh
+++ bin/init_db.sh
@@ -10,7 +10,7 @@ docker run \
   -p 5432:5432 \
   -v ~/tmp/postgres_data_stfs:/var/lib/postgresql/data \
   -e "POSTGRES_USER=stfs" \
-  postgres:latest
+  postgres:9.5
 
 echo Done \(container ID `docker ps -f name=pgserver -q`\)
 echo Data is persisted at ~/tmp/postgres_data_stfs and
