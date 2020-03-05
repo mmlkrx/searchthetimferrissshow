678ae8d3cefdb667a6e1fedfc769c3f053521c59
Matthias Michael Loker <m@theloker.com>
2020-03-04 21:29:58 -0800

    Render homepage at / path instead of /stfs
    I used /stfs initially because I was deploying this app at
    apps.mydomain.com/stfs and was planning on having each app
    live under it's own path. Never happened ¯\_(ツ)_/¯

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,9 @@
 class App < Rack::App
   extend Rack::App::FrontEnd
 
-  namespace '/stfs' do
-    get '/' do
-      @query    = params['q']
-      @episodes = FullTextSearch.find_episodes(@query)
-      render './views/index.html.erb'
-    end
+  get '/' do
+    @query    = params['q']
+    @episodes = FullTextSearch.find_episodes(@query)
+    render './views/index.html.erb'
   end
 end
