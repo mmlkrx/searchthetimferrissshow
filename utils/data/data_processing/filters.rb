cf3a17aad628a7c660c2440aaf672d6534b4e48c
Matthias Michael Loker <m@theloker.com>
2020-04-17 11:56:55 +0200

    Add broad transcript html url filter

--- utils/data/data_processing/filters.rb
+++ utils/data/data_processing/filters.rb
@@ -1,5 +1,6 @@
 module DataProcessing
   module Filters
     TRANSCRIPT_PDF_URL = '//a[starts-with(@href, "https://tim.blog/wp-content/uploads/")]'
+    TRANSCRIPT_HTML_URL = '//p/a[contains(@href, "transcript")]'
   end
 end
