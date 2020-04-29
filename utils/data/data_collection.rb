789d2c17f708411be605eca4e7cb77ee4261a0f6
Matthias Michael Loker <m@theloker.com>
2020-04-29 10:29:25 +0200

    Remove pdf download utility

--- utils/data/data_collection.rb
+++ utils/data/data_collection.rb
@@ -1,7 +1,6 @@
 module DataCollection
   module Config
     ROOT_DOWNLOAD_DIR = 'data/'
-    DOWNLOAD_DIR_PDF  = ROOT_DOWNLOAD_DIR + 'pdfs/'
     DOWNLOAD_DIR_WEB_PAGE  = ROOT_DOWNLOAD_DIR + 'web_pages/'
   end
 end
--- utils/data/data_processing/filters.rb
+++ utils/data/data_processing/filters.rb
@@ -1,6 +1,5 @@
 module DataProcessing
   module Filters
-    TRANSCRIPT_PDF_URL = '//a[starts-with(@href, "https://tim.blog/wp-content/uploads/")]'
     TRANSCRIPT_HTML_URL = '//p/a[contains(@href, "transcript")]'
     TRANSCRIPT_HTML_TEXT = '//div[@class="entry-content"]/p[strong[contains(text(), ":")]]'
   end
